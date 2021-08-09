e573ffa63f4893738e1a374ff0729e39443ffd60
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2021-08-09 22:43:54 +0800

    Delete data-raw directory

--- data-raw/usedata.R
+++
@@ -1,8 +0,0 @@
-nhs.path <- 'f:/Nhanes/'
-d5 <- c("Demographics", "Dietary", "Examination", "Laboratory", "Questionnaire")
-webyears <- c("2019-2020", "2017-2018", "2015-2016", "2013-2014", "2011-2012",
-              "2009-2010", "2007-2008", "2005-2006", "2003-2004", "2001-2002",
-              "1999-2000")
-nhs.path <- NULL
-usethis::use_data(d5,webyears,nhs.path,internal = TRUE,overwrite = TRUE)
-
