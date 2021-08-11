1b4a07f5bf38eba1516e776843941f858171f766
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2021-08-11 20:56:44 +0800

    Add files via upload

--- R/nhs_select.R
+++ R/nhs_select.R
@@ -3,12 +3,13 @@
 #' @param data result of nhs_read()
 #' @param ... paired names, connected by colon.
 #' @param select logical. whether to select the
+#' @param force_bind logical.
 #'
 #' @return
 #' @export
 #'
-nhs_select <- function(data,...){
-    names <- c(...)
+nhs_select <- function(data,...,force_bind=FALSE){
+    names <- c(...) |> tolower()
     # return(names)
     (from <- strsplit(names,' {0,}: {0,}') |> do::select(j=1) |> unlist())
     (select <- strsplit(from,',') |> unlist() |> unique())
@@ -25,15 +26,28 @@ nhs_select <- function(data,...){
         }
         data[[i]] <- di
     }
-    (ck <- sapply(2:length(data), function(i) do::increase(colnames(data[[1]])) == do::increase(colnames(data[[i]]))))
-    if (all(ck)){
-        for (i in 1:length(data)) {
-            di <- cbind(Year=names(data)[i],data[[i]])
-            data[[i]] <- di
-        }
-        names(data)=NULL
-        do.call(rbind,data) |> as.data.frame()
-    }else{
+    if (length(data)==1){
         data
+    }else{
+        (ck <- sapply(2:length(data), function(i) suppressWarnings(do::increase(colnames(data[[1]])) == do::increase(colnames(data[[i]])))))
+        if (all(ck)){
+            for (i in 1:length(data)) {
+                di <- cbind(Year=names(data)[i],data[[i]])
+                data[[i]] <- di
+            }
+            names(data)=NULL
+            do.call(rbind,data) |> as.data.frame()
+        }else{
+            if (force_bind){
+                for (i in 1:length(data)) {
+                    data[[i]] <- cbind(Year=names(data)[i],data[[i]])
+                }
+                do.call(plyr::rbind.fill,data)
+            }else{
+                data
+            }
+        }
     }
+
+
 }
